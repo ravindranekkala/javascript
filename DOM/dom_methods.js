bc84c1a5a4c0d05dac792262bfc237f068fe78a9
ravindra <ravinekkala8@gmail.com>
2023-09-05 12:05:49 +0530

    js

---
+++ DOM/dom_methods.js
@@ -0,0 +1,26 @@
+        // document.getElementById()
+
+  let ram= document.getElementById("k");
+  ram.style.fontSize = "xxx-large";
+     ram.innerHTML='ravis Js';
+     ram.style.color='forestgreen';
+   let ravi= document.getElementById('raju').src ='hmm.jpg';
+
+        // document.getElementsByClassName()
+
+   let krish=document.getElementsByClassName("sun");
+       krish[0].innerHTML="ravi"
+       krish[0].style.color="red"
+       krish[1].innerHTML="prasad"
+        krish[4].style.color="orange"
+       krish[3].innerHTML="rekha"
+
+       //document.getElementByTagName()
+
+       let am = document.getElementsByTagName("span");
+     am[0].style.backgroundColor = "forestgreen";
+     am[2].style.fontSize = "x-large";
+     am[1].style.color = "red";
+     //  for(i=0; i<am.length; i++){
+     // am[i].innerHTML='ravi';
+     //     }
